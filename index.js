a0e7e241986fd76c1a00c4ae0090a39f52fbaeb1
John Behan <behanj@gmail.com>
2020-10-28 19:43:27 +0000

    add check cookies function to check if cookie name and quote are present

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
-/**
- * Respond with hello worker text
- * @param {Request} request
- */
+function checkCookies(request, names) {
+  const cookies = request.headers.get('cookie');
+  console.log(cookies);
+  for (let name of names) {
+    if (!cookies.includes(`${name}=`)) {
+      return false;
+    }
+    return true;
+  };
+};
+
 async function handleRequest(request) {
+  const cookiesPresent = checkCookies(request, ['name', 'quote']);
+  console.log(cookiesPresent);
   return new Response('Hello worker!', {
     headers: { 'content-type': 'text/plain' },
   })
 }
+
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
